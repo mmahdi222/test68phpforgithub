6fa9072fea13a7ba4e339f2c0711480437566340
mmahdi222 <90545609+mmahdi222@users.noreply.github.com>
2021-09-29 09:11:45 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 </head>
 <body>
 <?php
-echo'<h1 style="color:red">test php for github pages</h1>'
+echo'<h1 style="color:red">test php for github pages</h1>';
+echo date(H:m:s);
 ?>
 </body>
 </html>
